0b01a00f976b23f04bdc37ce7b9c43aaeee2af01
Dinesh <dineshthiru.1998@gmail.com>
2020-01-10 16:10:22 +0530

    adding client and server

---
+++ client/.netlify/build/precache-manifest.dd512f51cc4ca38033f4999587f88850.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8f22bb243cbe947f6e8e4a0ffae5774",
+    "url": "/index.html"
+  },
+  {
+    "revision": "770ffdf8d96bd7a5f776",
+    "url": "/static/css/main.d89fdbca.chunk.css"
+  },
+  {
+    "revision": "c4e67b53d4e81c6d8815",
+    "url": "/static/js/2.2998d2d6.chunk.js"
+  },
+  {
+    "revision": "e5fdd3c10a39c54f9723efcb1214ee6a",
+    "url": "/static/js/2.2998d2d6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "770ffdf8d96bd7a5f776",
+    "url": "/static/js/main.cb917bb4.chunk.js"
+  },
+  {
+    "revision": "0281e3aa3ea9fc02c242",
+    "url": "/static/js/runtime-main.b12ff581.js"
+  }
+]);
